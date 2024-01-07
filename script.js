59e219f87e040ab49de8b521208d5b9fe982c125
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-07 16:39:13 +0330

    added checked and remove to tasks

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value="";
 }
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
